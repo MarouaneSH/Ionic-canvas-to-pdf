6213fd517d0252977744dff07989b901015954a3
Marouane Souah <marouanesouah@gmail.com>
2017-10-31 18:41:21 -0700

    Add ArrayBuffer for fixing Image Issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as jsPDF from 'jspdf';
@@ -25,34 +26,40 @@ export class HomePage {
 
   
   generatePdf(){
-    
-    html2canvas(document.getElementById("Html2Pdf"),{background:"white"}).then((canvas)=>{
-
+    const div = document.getElementById("Html2Pdf");
+    var doc = new jsPDF("p","mm","a4");
+    html2canvas(div,{background:"white",height :div.clientHeight , width : div.clientWidth  }).then((canvas)=>{
       //Initialize JSPDF
-      var doc = new jsPDF("p","mm","a4");
-      const width = doc.internal.pageSize.width;
-      const height = doc.internal.pageSize.height;
-      let imgData = canvas.toDataURL("image/PNG");
-
-      doc.addImage(imgData, 'PNG', 20,20,width,height);
-   
-
-      let pdfOutput = doc.output();
      
+      console.log("HEIGHt : 1  " +  div.offsetHeight + " Client : "+ div.clientHeight);
+      console.log("widht : 1  " +  div.offsetWidth + " Client : "+ div.offsetWidth);
+
+      //Converting canvas to Image
+      let imgData = canvas.toDataURL("image/PNG");
+      //Add image Canvas to PDF
+      doc.addImage(imgData, 'PNG', 20,20 );
+      let pdfOutput = doc.output();
+      let buffer = new ArrayBuffer(pdfOutput.length);
+      let array = new Uint8Array(buffer);
+      for (var i = 0; i < pdfOutput.length; i++) {
+          array[i] = pdfOutput.charCodeAt(i);
+      }
 
 
-      const directory = this.file.externalApplicationStorageDirectory  ;
-      const fileName = this.fileName + ".pdf";
+      //This is where the PDF file will stored , you can change it as you like
+      // for more information please visit https://ionicframework.com/docs/native/file/
+      const directory = this.file.externalApplicationStorageDirectory ;
+
+      //Name of pdf
+      const fileName = this.fileName+".pdf";
       
-      this.file.writeFile(directory,fileName,pdfOutput)
-      .then((success)=> console.log("succes" + JSON.stringify(success)))
-      .catch((error)=> console.log("errror" +JSON.stringify(error)));
+      //Writing File to Device
+      this.file.writeFile(directory,fileName,buffer)
+      .then((success)=> console.log("File created Succesfully" + JSON.stringify(success)))
+      .catch((error)=> console.log("Cannot Create File " +JSON.stringify(error)));
   
   
     });
-
-
-  
   }
 
 
